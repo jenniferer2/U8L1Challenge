fb376b51168d2296c98f18a5f9f5df50fe79b57a
BT_1E10_09 <dariusj0714@gmail.com>
2022-03-01 12:32:40 -0500

    Initial commit

---
+++ src/Challenge.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+public class Challenge {
+
+    public static void main (String [] args) {
+         String[][] seats = {
+                {"Moiz", "Angie", "Taran", "Kelvin", "Kyler", "David", "WenHao Huang", "Nicole", "Jennifer", "Michelle",null,null},
+                {"Beckett", "Raymond", "Lucy", "Apramjot", "Justin Lema", "Sam", "Tristan", "Pradeep", "Mohammad", "Haley", null, "Rely"},
+                {"Cheng Han", ",Qihan", ",Kevin", ",Ryan", ",Justin Liu", ",Jeffrey", ",Danny", ",Elliot", ",Benson", ",Fiona", ",Neil", "Kaitlyn"}
+        };
+         for (String [] x : seats) {
+             for (int i = 0; i < seats[0].length; i++ ) {
+                 String temp = x [i];
+                 
+
+
+
+             }
+
+         }
+
+
+
+
+
+    }
+}
+
