2daf6da44c8bed0175db7b60c87e4d716b624fdf
jennifer <xlin4004@bths.edu>
2022-03-05 19:49:40 -0500

    i finally did it

--- src/Challenge.java
+++ src/Challenge.java
@@ -2,50 +2,54 @@ import java.util.ArrayList;
 import java.util.Arrays;
 public class Challenge {
 
-    public static void main (String [] args) {
-         String[][] seats = {
-                {"Moiz", "Angie", "Taran", "Kelvin", "Kyler", "David", "WenHao Huang", "Nicole", "Jennifer", "Michelle",null,null},
-                {"Beckett", "Raymond", "Lucy", "Apramjot", "Justin Lema", "Sam", "Tristan", "Pradeep", "Mohammad", "Haley",  "Rely", null},
-                {"Cheng Han", ",Qihan", ",Kevin", ",Ryan", ",Justin Liu", ",Jeffrey", ",Danny", ",Elliot", ",Benson", ",Fiona", ",Neil", "Kaitlyn"}
+    public static void main(String[] args) {
+        String[][] seats = {
+                {"Moiz", "Angie", "Taran", "Kelvin", "Kyler", "David", "WenHao Huang", "Nicole", "Jennifer", "Michelle", null, null},
+                {"Beckett", "Raymond", "Lucy", "Apramjot", "Justin Lema", "Sam", "Tristan", "Pradeep", "Mohammad", "Haley", null, "Rely"},
+                {"Cheng Han", "Qihan", "Kevin", "Ryan", "Justin Liu", "Jeffrey", "Danny", "Elliot", "Benson", "Fiona", "Neil", "Kaitlyn"}
         };
 
-        String[][] seatNum =  new String[3][12];
-        ArrayList[] names =  ArrayList (0);
-        for (String [] x : seats) {
-            for (String y : x) {
-                names.add(y);
+
+        ArrayList<String> names = new ArrayList<String>();
+        String[][] news = new String[3][12];
+
+
+        for (int row = 0; row < seats.length; row++) {
+
+            for (int col = 0; col < seats[0].length; col++) {
+                names.add(seats[row][col]);
+
+            }
+
+        }
+
+        int diff = 36;
+
+
+        for (int r = 0; r < news.length; r++) {
+            for (int c = 0; c < news[0].length; c++) {
+                int num = (int) (Math.random() * diff);
+                news[r][c] = names.get(num);
+                names.remove(num);
+                diff--;
             }
         }
 
-        int x = 0 ;
-        int y = 0;
-        String hold = "";
-        for ()
 
-
-
-// start at first index of the seat num, grab a random person in seats and put that index. repeat teh process when there is already someone in that index or that index someo
-
-
-
-
-
-        for (String []na : seats) {
-          System.out.println(" \n"+ Arrays.toString(na));
+        for (String[] r : news) {
+            for (String c : r) {
+                System.out.print(c + " ");
+            }
         }
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        for (String []n : seatNum) {
-            System.out.println(Arrays.toString(n));
-        }
-
-
-
-
-
-
-
     }
 }
+
+
+
+
+
+
+
+
+
 
